a47ac1aa8333aca54f7689ae21dddb36619ba18b
stevens34400 <stevensisjayawan34400@gmail.com>
2021-06-08 16:30:53 -0400

    trying to set up Forms for user input

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './screens/orders_screen.dart';
+import './screens/edit_product_screen.dart';
+import './screens/user_products_screen.dart';
 import './providers/orders.dart';
 import './screens/cart_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -41,6 +43,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
+          UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
---
+++ shop_app/lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import '../screens/edit_product_screen.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, index) => Column(children: <Widget>[
+            UserProductItem(
+              title: productsData.items[index].title,
+              imageUrl: productsData.items[index].imageUrl,
+            ),
+            Divider(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
